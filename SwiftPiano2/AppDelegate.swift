33545316eef6fba919fd7427d943b46acfbf9574
TheMike <zeusintraining@gmail.com>
2014-06-10 15:19:05 -0400

    Initial Commit

---
+++ SwiftPiano2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftPiano2
+//
+//  Created by Michael Bullington on 6/10/14.
+//  Copyright (c) 2014 Michael Bullington. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftPiano2/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftPiano2
+//
+//  Created by Michael Bullington on 6/10/14.
+//  Copyright (c) 2014 Michael Bullington. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
